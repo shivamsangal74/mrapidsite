1f9c89ca332ed99828b682e495e1e48673812c3d
shivamsangal74 <shivamsangal74>
2026-09-15 15:29:18 +0530

    sfb

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import MrapidHero from './components/MrapidHero';
 import MrapidOptionCards from './components/MrapidOptionCards';
@@ -125,6 +125,14 @@ export default function App() {
         <Route
           path="/terms"
           element={<TermsWrapper onOpenDownloadModal={handleOpenDownloadModal} />}
+        />
+        <Route
+          path="/app"
+          element={<Navigate to="/" replace />}
+        />
+        <Route
+          path="/app/*"
+          element={<Navigate to="/" replace />}
         />
       </Routes>
 
